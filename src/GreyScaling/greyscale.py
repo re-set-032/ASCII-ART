3617f8c5ed76ebd56a908e9213f20a69124b1aa6
ronin117-prog <namelessking032@gmail.com>
2020-08-01 23:15:10 +0530

    Added and refactored greyscaling and art codes

--- src/GreyScaling/greyscale.py
+++ src/GreyScaling/greyscale.py
@@ -1,11 +1,13 @@
 # Importing Image and ImageOps module from PIL package
 from PIL import Image, ImageOps
 
-# creating an og_image object
-og_image = Image.open("birdies.jpeg")
-og_image.show()
+if __name__ == "__main__":
 
-# applying grayscale method
-gray_image = ImageOps.grayscale(og_image)
-gray_image.save('gray_image.png')
-gray_image.show()
+    # Reading RGB image (Change image file here)
+    original_image = Image.open("sample.jpg)
+    # original_image.show()
+
+    # Applying grayscale method
+    gray_image = ImageOps.grayscale(original_image)
+    gray_image.save('grey.png')
+    # gray_image.show()
